85c7d8aa401a0ce5f6ffb5d709a12fd0f0feb502
skrustev <striker18@mail.bg>
2016-07-03 14:38:44 +0300

    Add second multiple pages to the GUI. Add single image testing with multiple training and options for import and export of neural network.

--- gui.py
+++ gui.py
@@ -1,60 +1,248 @@
-import tkinter
-from tkinter import Frame, X, LEFT, CENTER, BOTTOM, BOTH
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog, Frame, X, LEFT, RIGHT, CENTER, BOTTOM, BOTH
 import neural_network
+from neural_network import Brain
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
+		
+		
+class App(tk.Tk):
 
-#Window settings
-widthpixels = 500
-heightpixels = 500
+	def __init__(self, *args, **kwargs):
+		
+		tk.Tk.__init__(self, *args, **kwargs)
+		
+		#Window settings
+		widthpixels = 500
+		heightpixels = 500
 
-screen = tkinter.Tk()
-screen.wm_title("Traffic Sign Recognition")
-screen.geometry('{}x{}'.format(widthpixels, heightpixels))
-screen.resizable(width=True, height=True)
+		tk.Tk.wm_title(self, "Traffic Sign Recognition")
+		tk.Tk.geometry(self, '{}x{}'.format(widthpixels, heightpixels))
+		tk.Tk.resizable(self, width=True, height=True)
 
-#Frame 1 - set data
-frame1 = Frame(screen)
-frame1.pack(fill=X)
+		container = tk.Frame(self)
+		container.pack(side="top", fill="both", expand = True)
+		container.grid_rowconfigure(0, weight=1)
+		container.grid_columnconfigure(0, weight=1)
 
-label = tkinter.Label(frame1, text="Epochs:")
-#label.place(relx=0.15, rely = 0.05, anchor = CENTER)
-label.pack(side=LEFT)
-entry = tkinter.Entry(frame1, bd = 2)
-#entry.place(relx=0.45, rely = 0.05, anchor = CENTER)
-entry.pack(side=LEFT)
-button = tkinter.Button(frame1, text="Start", command = lambda: callback(int(entry.get())))
-#button.place(relx=0.75, rely = 0.05, anchor = CENTER)
-button.pack(side=LEFT)
+		self.frames = {}
+		self.widths = {}
+		self.heights = {}
+		
+		for F in (StartPage, PageOne, PageTwo):
 
-#Frame 2 - graph
-frame2 = Frame(screen)
-frame2.pack(fill=X)
+			frame = F(container, self)
 
-x_axis = [0]
-y_axis = [100]
+			self.frames[F] = frame
+			self.widths[F] = frame.width
+			self.heights[F] = frame.height
 
-fig = Figure(figsize=(5,5), dpi=100)
-ax = fig.add_subplot(111)
-ax.axis([0,30,0,100])
-ax.set_xlabel('Num of epochs')
-ax.set_ylabel('Failed')
-line1, = ax.plot(x_axis, y_axis)
+			frame.grid(row=0, column=0, sticky="nsew")
 
-canvas = FigureCanvasTkAgg(fig, frame2)
-toolbar = NavigationToolbar2TkAgg(canvas, frame2)
-toolbar.pack(side=BOTTOM)
-canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+		self.show_frame(StartPage)
+
+	def show_frame(self, page_name):
+
+		for frame in self.frames.values():
+			frame.grid_remove()
+		frame = self.frames[page_name]
+		tk.Tk.geometry(self, '{}x{}'.format(self.widths[page_name], self.heights[page_name]))
+		frame.grid()
 	
-def callback(epochs):
-	failed = neural_network.train(epochs)
-	x_axis.append(epochs)
-	y_axis.append(failed)
-	line1.set_xdata(x_axis)
-	line1.set_ydata(y_axis)
-	fig.canvas.draw()
+	def change_res(self, width, height):
+		tk.Tk.geometry(self, '{}x{}'.format(width, height))
 
-screen.mainloop()
+class StartPage(tk.Frame):
+
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self,parent)
+		self.width = 300
+		self.height = 100
+		label = tk.Label(self, text="Start Page")
+		label.pack(pady=10,padx=10)
+
+		button = ttk.Button(self, text="Epochs graph",
+							command=lambda: controller.show_frame(PageOne))
+		button.pack()
+
+		button2 = ttk.Button(self, text="Test image",
+							command=lambda: controller.show_frame(PageTwo))
+		button2.pack()
+
+class PageOne(tk.Frame):
+
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		self.width = 500
+		self.height = 500
+		label = tk.Label(self, text="Graph Page!")
+		label.pack(pady=10,padx=10)
+		
+		button1 = ttk.Button(self, text="Back to Home",
+					command=lambda: controller.show_frame(StartPage))
+		button1.pack()
+		
+		#Frame 1 - set data
+		frame1 = Frame(self)
+		frame1.pack(fill=X)
+
+		frame1_center = Frame(frame1)
+		frame1_center.pack()
+		
+		label = ttk.Label(frame1_center, text="Epochs:")
+		label.pack(side=LEFT, pady=10,padx=10)
+		entry = tk.Entry(frame1_center, bd = 2)
+		entry.pack(side=LEFT, pady=10,padx=10)
+		button = ttk.Button(frame1_center, text="Test", command = lambda: self.callback(int(entry.get())))
+		button.pack(side=LEFT, pady=10,padx=10)
+		
+		#Frame 2 - graph
+		frame2 = Frame(self)
+		frame2.pack(fill=X)
+
+		self.x_axis = [0]
+		self.y_axis = [100]
+
+		self.fig = Figure(figsize=(5,5), dpi=100)
+		ax = self.fig.add_subplot(111)
+		ax.axis([0,30,0,100])
+		ax.set_xlabel('Num of epochs')
+		ax.set_ylabel('Failed')
+		self.line1, = ax.plot(self.x_axis, self.y_axis)
+
+		canvas = FigureCanvasTkAgg(self.fig, frame2)
+		toolbar = NavigationToolbar2TkAgg(canvas, frame2)
+		toolbar.pack(side=BOTTOM)
+		canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+		
+		#brain
+		self.brain = Brain()
+		
+	def callback(self, epochs):
+		failed = self.brain.test_train(epochs)
+		self.x_axis.append(epochs)
+		self.y_axis.append(failed)
+		self.line1.set_xdata(self.x_axis)
+		self.line1.set_ydata(self.y_axis)
+		self.fig.canvas.draw()
+		print("Num failed: " + str(failed))
+
+class PageTwo(tk.Frame):
+
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		self.width = 500
+		self.height = 500
+		label = tk.Label(self, text="Test image!")
+		label.pack(pady=10,padx=10)
+
+		button1 = ttk.Button(self, text="Back to Home",
+							command=lambda: controller.show_frame(StartPage))
+		button1.pack()
+		
+		#Frame - Network actions
+		frame_net = Frame(self)
+		frame_net.pack(fill=X)
+		
+		label_epochs = ttk.Label(frame_net, text="Network: ")
+		label_epochs.pack(side=LEFT, pady=10,padx=10)
+		
+		button = ttk.Button(frame_net, text="Import", command=self.ask_import_net)
+		button.pack(side=LEFT)
+		
+		button = ttk.Button(frame_net, text="Export", command=self.ask_export_net)
+		button.pack(side=LEFT)
+		
+		#Frame 1 - set data
+		frame1 = Frame(self)
+		frame1.pack(fill=X)
+		
+		label_epochs = ttk.Label(frame1, text="Epochs:")
+		label_epochs.pack(side=LEFT, pady=10,padx=10)
+		entry_epochs = tk.Entry(frame1, bd = 2)
+		entry_epochs.pack(side=LEFT, pady=10,padx=10)
+		button = ttk.Button(frame1, text="Clean Train", command = lambda: self.train_clean(int(entry_epochs.get())))
+		button.pack(side=LEFT, pady=10,padx=10)
+
+		button = ttk.Button(frame1, text="Train", command = lambda: self.train(int(entry_epochs.get())))
+		button.pack(side=LEFT, pady=10,padx=10)
+		
+		#Frame 2 - set data
+		frame2 = Frame(self)
+		frame2.pack(fill=X)
+		
+		label_file = ttk.Label(frame2, text="None")
+		
+		button = ttk.Button(frame2, text="File", command=lambda: self.askopenfile(label_file))
+		button.pack(side=LEFT)
+		
+		label_file.pack(side=LEFT, pady=10,padx=10)
+		
+		#Frame - Test button
+		frame_test = Frame(self)
+		frame_test.pack(fill=X)
+		
+		button = ttk.Button(frame_test, text="Test", command=self.test_image)
+		button.pack(side=LEFT)
+		
+		#Frame - Result label
+		frame_rl = Frame(self)
+		frame_rl.pack(fill=X)
+		
+		label_r = ttk.Label(frame_rl, text="Result:")
+		label_r.pack(side=LEFT, pady=10,padx=10)
+	
+		#Frame - Result frame
+		frame_result = Frame(self)
+		frame_result.pack(fill=X)
+		
+		self.label_result = ttk.Label(frame_result, text="Sign: None, Output number: None")
+		self.label_result.pack(side=LEFT, pady=10,padx=10)
+		
+		#brain
+		self.image_file = ""
+		self.network_file = ""
+		self.brain = Brain()
+		
+	def askopenfile(self, in_label):
+		self.image_file = filedialog.askopenfilename()
+		
+		if self.image_file:
+			print("Opened file: " + self.image_file)
+			in_label["text"] = self.image_file
+			
+	def ask_import_net(self):
+		self.network_file = filedialog.askopenfilename(filetypes = [("XML files", "*.xml")])
+			
+		if self.network_file:
+			self.brain.import_network(self.network_file)
+			print("Imported network from file: " + self.network_file)
+	
+	def ask_export_net(self):
+		self.network_file = filedialog.asksaveasfilename(filetypes = (("XML files", "*.xml")))
+		
+		if self.network_file:
+			self.brain.export_network(self.network_file)
+			print("Exported network to file: " + self.filename)
+	
+	def train_clean(self, epochs):
+		self.brain.train_clean(epochs)
+		
+	def train(self, epochs):
+		self.brain.train_more(epochs)
+		
+	def test_image(self):
+		sign_names = ["20", "30", "50", "60", "70", "80", "80 out", "100", "120",
+						"S predimstvo", "Bez predimstvo", "STOP", "B2 - Zabraneno vlizaneto v dvete posoki",
+						"В1 - zabranetno vlizaneto", "A32 - uchastuk v remont"]
+								
+		result = self.brain.test_image(self.image_file)
+		sign_name = sign_names[abs(int(round(result)))]
+		self.label_result["text"] = "Sign: " + sign_name + ", Output number: " + str(result)
+
+app = App()
+app.mainloop()
 
--- neural_network.py
+++ neural_network.py
@@ -3,62 +3,124 @@ import numpy as np
 import csv
 from pybrain.datasets.supervised import SupervisedDataSet 
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.supervised.trainers import BackpropTrainer, RPropMinusTrainer
+from pybrain.tools.customxml import NetworkWriter
+from pybrain.tools.customxml import NetworkReader
 
-def train(epochs=1):
-    classes = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 17, 25])
-    samples = []
-    labels = []
-    for i in range(len(classes)):
-        prefix = "GTSRB/" + format(classes[i], '05d') + '/'
-        file = open(prefix + 'GT-' + format(classes[i], '05d') + '.csv')
-        reader = csv.reader(file, delimiter=';')
-        next(reader, None)
+class Brain:		
+	def __init__(self):
+		classes = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 13, 14, 15, 17, 25])
+		self.samples = []
+		self.labels = []
+		for i in range(len(classes)):
+			prefix = "GTSRB/" + format(classes[i], '05d') + '/'
+			file = open(prefix + 'GT-' + format(classes[i], '05d') + '.csv')
+			reader = csv.reader(file, delimiter=';')
+			next(reader, None)
 
-        for row in reader:
-            image = cv2.imread(prefix + row[0])
-            samples.append(image)
-            labels.append(i)
+			for row in reader:
+				image = cv2.imread(prefix + row[0])
+				self.samples.append(image)
+				self.labels.append(i)
 
-    samples = [cv2.resize(s, (10, 10)) for s in samples]
-    samples = np.array(samples).astype(np.float32) / 255
-    samples = [s.flatten() for s in samples]
+		self.samples = [cv2.resize(s, (10, 10)) for s in self.samples]
+		self.samples = np.array(self.samples).astype(np.float32) / 255
+		self.samples = [s.flatten() for s in self.samples]
+		
+		np.random.seed(0)
+		np.random.shuffle(self.samples)
+		np.random.seed(0)
+		np.random.shuffle(self.labels)
+	
+		self.totalEpochs = 0
 
-    np.random.seed(0)
-    np.random.shuffle(samples)
-    np.random.seed(0)
-    np.random.shuffle(labels)
+	def test_train(self, epochs=1):
+		print("Training...")
 
-    split = int(len(samples) * 0.7)
-    train_samples = samples[0:split]
-    train_labels  = labels[0:split]
+		split = int(len(self.samples) * 0.7)
+		train_samples = self.samples[0:split]
+		train_labels  = self.labels[0:split]
 
-    test_samples = samples[split:]
-    test_labels  = labels[split:]
+		test_samples = self.samples[split:]
+		test_labels  = self.labels[split:]
 
-    net = buildNetwork(300, 300, 1)
-    ds = SupervisedDataSet(300, 1)
-    for i in range(len(train_samples)):  
-        ds.addSample(tuple(np.array(train_samples[i], dtype='float64')), (train_labels[i],))
+		net = buildNetwork(300, 300, 1)	
+		ds = SupervisedDataSet(300, 1)
+		for i in range(len(train_samples)):  
+			ds.addSample(tuple(np.array(train_samples[i], dtype='float64')), (train_labels[i],))
+		
+		trainer = BackpropTrainer(net, ds, verbose=True)
+		trainer.trainEpochs(epochs)
+		self.totalEpochs = epochs
+		
+		error = 0
+		counter = 0
+		for i in range(0, 100):
+			output = net.activate(tuple(np.array(test_samples[i], dtype='float64')))
+			if round(output[0]) != test_labels[i]:
+				counter += 1
+				print(counter, " : output : ", output[0], " real answer : ", test_labels[i])
+				error += 1
+			else:
+				counter += 1
+				print(counter, " : output : ", output[0], " real answer : ", test_labels[i])
+		
+		print("Trained with " + str(epochs) + " epochs; Total: " + str(self.totalEpochs) + ";")
+		return error
+	
+	def train_clean(self, epochs=1):
+		print("Training...")
+		self.totalEpochs = epochs
+		
+		train_samples = self.samples
+		train_labels  = self.labels
 
-    #for inpt, target in ds:
-    #    print(inpt, target)
-    
-    trainer = BackpropTrainer(net, ds)
-    trainer.trainEpochs(epochs)
+		self.net_shared = buildNetwork(300, 300, 1)	
+		self.ds_shared = SupervisedDataSet(300, 1)
+		for i in range(len(train_samples)):  
+			self.ds_shared.addSample(tuple(np.array(train_samples[i], dtype='float64')), (train_labels[i],))
+		
+		self.trainer_shared = BackpropTrainer(self.net_shared, self.ds_shared, verbose=True)
+		self.trainer_shared.trainEpochs(epochs)
+		
+		print("Trained with " + str(epochs) + " epochs; Total: " + str(self.totalEpochs) + ";")
 
-    error = 0
-    counter = 0
-    for i in range(0, 100):
-        output = net.activate(tuple(np.array(test_samples[i], dtype='float64')))
-        if round(output[0]) != test_labels[i]:
-            counter += 1
-            print(counter, " : output : ", output[0], " real answer : ", test_labels[i])
-            error += 1
-        else:
-            counter += 1
-            print(counter, " : output : ", output[0], " real answer : ", test_labels[i])
-    return error
+	def train_more(self, epochs=1):
+		print("Training...")
+		self.totalEpochs += epochs
+		self.trainer_shared.trainEpochs(epochs)
+		print("Trained with " + str(epochs) + " epochs more; Total: " + str(self.totalEpochs) + ";")
+	
+	def test_image(self, filename):		
+		image = cv2.imread(filename)
+		images = [image]
+		
+		images = [cv2.resize(s, (10, 10)) for s in images]
+		images = np.array(images).astype(np.float32) / 255
+		images = [s.flatten() for s in images]
+		
+		output = self.net_shared.activate(tuple(np.array(images[0], dtype='float64')))
+		print("Output: ", output[0])
+		return output[0]
+			
+	def import_network(self, filename):
+		train_samples = self.samples
+		train_labels  = self.labels
+		
+		np.random.seed(0)
+		np.random.shuffle(train_samples)
+		np.random.seed(0)
+		np.random.shuffle(train_labels)
+		
+		self.net_shared = NetworkReader.readFrom(filename)
+		self.ds_shared = SupervisedDataSet(300, 1)
+		for i in range(len(train_samples)):  
+			self.ds_shared.addSample(tuple(np.array(train_samples[i], dtype='float64')), (train_labels[i],))
+			
+		self.trainer_shared = BackpropTrainer(self.net_shared, self.ds_shared, verbose=True)
+		
+	def export_network(self, filename):
+		NetworkWriter.writeToFile(self.net_shared, filename)
 
 
 
